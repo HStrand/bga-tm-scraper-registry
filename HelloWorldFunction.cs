3b0fa340b68d963e1e199b87af99aed41bd36a77
Håkon Hapnes Strand <haakon@headroom.as>
2025-07-18 20:59:13 +0200

    Change response in HelloWorldFunction

--- HelloWorldFunction.cs
+++ HelloWorldFunction.cs
@@ -19,15 +19,7 @@ namespace BgaTmScraperRegistry
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+            string responseMessage = "Hello from Terraforming Mars BGA Scraper API!";
 
             return new OkObjectResult(responseMessage);
         }
